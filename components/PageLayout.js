6d9e4362eb85d6ca2d0a6e54f8cf2a8b3be8c5b1
Graham Wright <graham@opusaffair.com>
2018-11-18 23:01:19 -0500

    delete footer

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -3,7 +3,6 @@ import { MuiThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Theme from "./Theme";
 import Header from "./Header";
-import Footer from "./Footer";
 
 class PageLayout extends Component {
   render() {
