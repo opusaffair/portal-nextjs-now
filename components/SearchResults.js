d79124ee5eb2ae2d7400c0b764f7e85421602746
Graham Wright <graham@opusaffair.com>
2018-11-28 10:35:29 -0500

    style pagination

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -5,12 +5,40 @@ import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
 import SearchResultGrid from "./SearchResultGrid";
 
-const styles = theme => {};
+const styles = theme => ({
+  pagination: {
+    marginTop: theme.spacing.unit * 5,
+    marginBottom: theme.spacing.unit * 5,
+    display: "flex",
+    justifyContent: "center",
+    "& .ais-Pagination": {
+      fontFamily: theme.typography.fontFamily
+    },
+    "& .ais-Pagination--noRefinement": {},
+    "& .ais-Pagination-list": {},
+    "& .ais-Pagination-list--noRefinement": {},
+    "& .ais-Pagination-item": {},
+    "& .ais-Pagination-item--firstPage": {},
+    "& .ais-Pagination-item--lastPage": {},
+    "& .ais-Pagination-item--previousPage": {},
+    "& .ais-Pagination-item--nextPage": {},
+    "& .ais-Pagination-item--page": {},
+    "& .ais-Pagination-item--selected": {},
+    "& .ais-Pagination-item--disabled": {},
+    "& .ais-Pagination-link": {
+      color: theme.palette.secondary.main
+    },
+    "& .ais-Pagination-link--selected": {
+      backgroundColor: theme.palette.primary.main,
+      border: "none"
+    }
+  }
+});
 
-const CustomHits = ({ hits, marginLeft, hasMore, refines }) => (
+const CustomHits = ({ hits, classes }) => (
   <Fragment>
     <SearchResultGrid hits={hits} />
-    <div style={{ display: "flex", justifyContent: "center" }}>
+    <div className={classes.pagination}>
       <Pagination />
     </div>
   </Fragment>
