c5badf8dde91005685a1e1288ac98ac4821b37d1
Graham Wright <graham@opusaffair.com>
2019-02-28 18:10:54 -0500

    add logos on about

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
 const styles = theme => ({
   root: {
@@ -25,71 +26,124 @@ const styles = theme => ({
   paper: {
     ...theme.mixins.gutters(),
     padding: theme.spacing.unit
+  },
+  logos: {
+    maxWidth: "200px"
   }
 });
 
 const About = ({ classes }) => (
   <div className="container" className={classes.root}>
-    <Typography variant="h1" gutterBottom>
-      StagePage
-    </Typography>
-    <Typography paragraph>
-      Welcome to the new StagePage, your digital portal to New England Theatre.
-      Want to know what’s good in Rhode Island,looking for a little night music
-      up in Maine, well you’re on the right track. Browse through hundreds of
-      theatre listings that will take you all the way from Saturday night to
-      Sunday morning and beyond. If you’re looking for theatre in New England,
-      your first stop needs to be the StagePage!
-    </Typography>
-    <Typography variant="h3">
-      <img src="/static/stagesource-logo.png" width="350" alt="StageSource" />
-    </Typography>
-    <Typography paragraph>
-      StageSource provides leadership and services to advance the art of theater
-      in the Greater New England region, uniting theater artists, theater
-      companies, and collaborators in vision and goals that inspire and empower
-      our community to realize its greatest artistic potential through workforce
-      development and sector improvement. Want to know more? Visit us at{" "}
-      <a href="https://www.stagesource.org" target="_blank">
-        stagesource.org
-      </a>
-    </Typography>
-    <Button
-      variant="contained"
-      color="primary"
-      href="https://www.stagesource.org/"
-    >
-      Become a StageSource Member
-    </Button>
-    {/* <Paper className={classes.paper}> */}
-    <br />
-    <Typography variant="h3">
-      <img src="/static/opusaffair-logo.png" alt="Opus Affair" width="350" />
-    </Typography>
+    <Grid container justify="center">
+      <Grid item md={6}>
+        <Typography variant="h1" gutterBottom>
+          StagePage
+        </Typography>
+        <Typography paragraph>
+          Welcome to the new StagePage, your digital portal to New England
+          Theatre. Want to know what’s good in Rhode Island,looking for a little
+          night music up in Maine, well you’re on the right track. Browse
+          through hundreds of theatre listings that will take you all the way
+          from Saturday night to Sunday morning and beyond. If you’re looking
+          for theatre in New England, your first stop needs to be the StagePage!
+        </Typography>
+        <Typography variant="h3">
+          <img
+            src="/static/stagesource-logo.png"
+            width="350"
+            alt="StageSource"
+          />
+        </Typography>
+        <Typography paragraph>
+          StageSource provides leadership and services to advance the art of
+          theater in the Greater New England region, uniting theater artists,
+          theater companies, and collaborators in vision and goals that inspire
+          and empower our community to realize its greatest artistic potential
+          through workforce development and sector improvement. Want to know
+          more? Visit us at{" "}
+          <a href="https://www.stagesource.org" target="_blank">
+            stagesource.org
+          </a>
+        </Typography>
 
-    <Typography paragraph>
-      Opus Affair built this StagePage site as part of their Common Calendar
-      project, building a shared event database to power arts event calendars,
-      like StagePage, the{" "}
-      <a href="https://www.bostonoperacalendar.org/" target="_blank">
-        Boston Opera Calendar
-      </a>
-      , and the{" "}
-      <a href="https://www.opusaffair.com/calendar/" target="_blank">
-        Opus Affair Calendar
-      </a>
-      . They also host a monthly networking event for anyone interested in the
-      arts in Boston—and provide design and digital marketing services to arts
-      and culture organizations across the country.
-    </Typography>
-    <Button
-      variant="contained"
-      color="primary"
-      href="https://www.opusaffair.com"
-    >
-      Learn More About Opus Affair
-    </Button>
-    {/* </Paper> */}
+        <Button
+          variant="contained"
+          color="primary"
+          href="https://www.stagesource.org/"
+        >
+          Become a StageSource Member
+        </Button>
+
+        <br />
+        <Typography variant="h3">
+          <img
+            src="/static/opusaffair-logo.png"
+            alt="Opus Affair"
+            width="350"
+          />
+        </Typography>
+        <Typography paragraph>
+          Opus Affair built this StagePage site as part of their Common Calendar
+          project, building a shared event database to power arts event
+          calendars, like StagePage, the{" "}
+          <a href="https://www.bostonoperacalendar.org/" target="_blank">
+            Boston Opera Calendar
+          </a>
+          , and the{" "}
+          <a href="https://www.opusaffair.com/calendar/" target="_blank">
+            Opus Affair Calendar
+          </a>
+          . They also host a monthly networking event for anyone interested in
+          the arts in Boston—and provide design and digital marketing services
+          to arts and culture organizations across the country.
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          href="https://www.opusaffair.com"
+        >
+          Learn More About Opus Affair
+        </Button>
+        <br />
+
+        <Typography variant="h3">About Standing O</Typography>
+        <Typography paragraph>
+          An event qualifies for a Standing O if it meets three of the following
+          five criteria: Female Director, Female Playwright, At least 50% Female
+          Cast, At least 50% Female Design Team, or Female-Centered Story
+        </Typography>
+        <Typography variant="h3" gutterBottom>
+          StageSource Partners
+        </Typography>
+
+        <Grid container justify="center" spacing={16}>
+          <Grid item md={4}>
+            <img
+              className={classes.logos}
+              src="/static/MCC_Logo_RGB_NoTag.jpg"
+              width="100%"
+              alt="Massachusetts Cultural Council"
+            />
+          </Grid>
+          <Grid item md={4}>
+            <img
+              className={classes.logos}
+              src="/static/BCC Logo.jpg"
+              width="100%"
+              alt="Boston Cultural Council"
+            />
+          </Grid>
+          <Grid item md={4}>
+            <img
+              className={classes.logos}
+              src="/static/TBF_stacked_rgb_Screen.png"
+              width="100%"
+              alt="The Boston Foundation"
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   </div>
 );
 
